4e22a70bf91ca55d922c1819bb08a228a742deab
sourav guha roy <souravroy94375@gmail.com>
2024-09-15 17:17:33 +0530

    add

--- src/app/(customerFacing)/Popular/Popular.tsx
+++ src/app/(customerFacing)/Popular/Popular.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ProductCard, { ProductCardSkeleton } from "@/components/ProductCard";
 import { Button } from "@/components/ui/button";
-import { Product } from "@prisma/client";
 import Link from "next/link";
-
 import { Suspense } from "react";
+import db from "@/db/db"; // Import your Prisma client
+
+// Infer Product type from Prisma client
+type Product = Awaited<ReturnType<typeof db.product.findMany>>[number];
 
 type ProductGridSectionProps = {
   title: string;
   productFetcher: () => Promise<Product[]>;
 };
+
 export default function Popular({
   productFetcher,
   title,
@@ -18,7 +21,7 @@ export default function Popular({
     <div>
       <div className="space-y-4 px-3">
         <div className="flex gap-4 items-center justify-center">
-          <h2 className="text-[5.9vh] tracking-tighter  uppercase">{title}</h2>
+          <h2 className="text-[5.9vh] tracking-tighter uppercase">{title}</h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-8 gap-4">
           <Suspense
@@ -34,14 +37,14 @@ export default function Popular({
           </Suspense>
         </div>
       </div>
-      <div className="flex  mt-14  items-center  justify-center">
+      <div className="flex mt-14 items-center justify-center">
         <Button
           variant="outline"
-          className="border-zinc-900 px-8 hover:shadow-md py-4 border-2 "
+          className="border-zinc-900 px-8 hover:shadow-md py-4 border-2"
           asChild
         >
-          <Link href="/products" className="space-x-2 my-10 ">
-            <span className="text-base ">View All</span>
+          <Link href="/products" className="space-x-2 my-10">
+            <span className="text-base">View All</span>
             {/* <ArrowRight className="size-4" /> */}
           </Link>
         </Button>
@@ -49,12 +52,14 @@ export default function Popular({
     </div>
   );
 }
+
 async function ProductSuspense({
   productFetcher,
 }: {
   productFetcher: () => Promise<Product[]>;
 }) {
-  return (await productFetcher()).map((product) => (
+  const products = await productFetcher();
+  return products.map((product) => (
     <ProductCard key={product.id} {...product} />
   ));
 }
