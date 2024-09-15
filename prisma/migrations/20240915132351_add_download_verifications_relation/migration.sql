7729a70d6c9ea259fd590611cd3a36313b0c9774
sourav guha roy <souravroy94375@gmail.com>
2024-09-15 19:09:53 +0530

    everything

---
+++ prisma/migrations/20240915132351_add_download_verifications_relation/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Cart" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Cart" ("createdAt", "id", "updatedAt", "userId") SELECT "createdAt", "id", "updatedAt", "userId" FROM "Cart";
+DROP TABLE "Cart";
+ALTER TABLE "new_Cart" RENAME TO "Cart";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const PrismaClientSingleton = () =>{
-  return new PrismaClient()
+// Function to create a new PrismaClient instance
+const PrismaClientSingleton = () => new PrismaClient();
+
+declare global {
+  var prisma: PrismaClient | undefined;
 }
-declare global{
-  var prisma:undefined | ReturnType<typeof PrismaClientSingleton>
+
+// Create a singleton Prisma Client instance
+const db = globalThis.prisma ?? PrismaClientSingleton();
+
+// Store the instance globally in development mode to avoid multiple instances
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = db;
 }
-const db = globalThis.prisma ?? PrismaClientSingleton()
 
-export default db 
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
+export default db;
