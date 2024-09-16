aa74976a7026e3ea7bf02bc3722d013d0aeaaf20
sourav guha roy <souravroy94375@gmail.com>
2024-09-16 12:24:59 +0530

    new update

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,18 +1,33 @@
+// import { PrismaClient } from '@prisma/client';
+
+// // Function to create a new PrismaClient instance
+// const PrismaClientSingleton = () => new PrismaClient();
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// // Create a singleton Prisma Client instance
+// const db = globalThis.prisma ?? PrismaClientSingleton();
+
+// // Store the instance globally in development mode to avoid multiple instances
+// if (process.env.NODE_ENV !== 'production') {
+//   globalThis.prisma = db;
+// }
+
+// export default db;
 import { PrismaClient } from '@prisma/client';
 
-// Function to create a new PrismaClient instance
-const PrismaClientSingleton = () => new PrismaClient();
-
+// Avoid multiple instances of PrismaClient in development
 declare global {
+  // Ensure that 'prisma' is only available in a Node.js global context
   var prisma: PrismaClient | undefined;
 }
 
-// Create a singleton Prisma Client instance
-const db = globalThis.prisma ?? PrismaClientSingleton();
+const db = global.prisma || new PrismaClient();
 
-// Store the instance globally in development mode to avoid multiple instances
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = db;
+  global.prisma = db; // In development, store Prisma client in globalThis
 }
 
 export default db;
